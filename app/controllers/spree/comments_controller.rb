7e73d15d2580ce6010108f96e88f62852f38d2df
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-11-03 11:40:06 -0200

    Remove problematic authorize! & redirect to slug instead of product id

--- app/controllers/spree/comments_controller.rb
+++ app/controllers/spree/comments_controller.rb
@@ -4,12 +4,11 @@ module Spree
       # Spree::StoreController already has authenticaiton methods
       # like you can see here
       # https://github.com/spree/spree/blob/v2.3.1/core/lib/spree/core/controller_helpers/auth.rb#L12
-      authorize!(:create, Comment)
       @comment      = Spree::Comment.new(permitted_params)
       @comment.user = try_spree_current_user
       # Set the message depending on the state of the .save method
       @comment.save ? (flash[:success] = t('spree.comment.success')) : (flash[:error] = t('spree.comment.failure'))
-      redirect_to spree.product_path(@comment.product_id || @comment.commentable.product_id)
+      redirect_to product_path(@comment.product.slug || @comment.commentable.product.slug)
     end
 
     protected
